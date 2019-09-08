32297a0551fcbc18492c4c98cfd28931eca6fce3
Kim Jharles <globstared@gmail.com>
2019-09-08 12:13:52 -0400

    playing with dics

--- browzers.py
+++ browzers.py
@@ -2,7 +2,26 @@
 
 # browzers
 
-browesers = ["firefox","chrome","brave", "internet explorer", "vivida",]
+browzer = ["firefox","chrome","brave", "internet explorer", "vivida",]
 
-for x in browesers:
+for x in browzer:
+   print(x)
+print("-----------")
+browzer2 = {   
+    "mozilla" : "firefox",
+    "google"  : "chrome",
+    "??" : "brave",
+    "??"  : "vivida",
+    "microsoft" : "internet explorer",
+    "apple" : "safari",
+}
+
+for x in browzer2:
     print(x)
+    
+    
+print("-----------")
+
+
+for key,val in browzer2.items():
+    print(key, "=>", val)
